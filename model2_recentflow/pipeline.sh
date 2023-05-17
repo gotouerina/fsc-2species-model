3c62349df32eae3ef5e8f4da16ea46c7843da17e
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-05-17 15:48:58 +0800

    Update pipeline.sh

--- model2_recentflow/pipeline.sh
+++ model2_recentflow/pipeline.sh
@@ -1,1 +1,4 @@
-00
+for i in {0..99}
+do
+  echo -e "mkdir run$i; cp model2.est model2.tpl model2_jointDAFpop1_0.obs run$i; cd run$i; fsc2709  -t model2.tpl -e model2.est -d -n 100000 -N 100000 -L 20 -c0 -B30 -l10 -M -q  ; cd .." >> model2.sh
+done
